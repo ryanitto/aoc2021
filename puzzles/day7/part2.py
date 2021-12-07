2a8a5adfd8198a5d9e7b6d13d465be8bd76add21
Ryan Metcalf <ryanitto@gmail.com>
2021-12-07 08:04:38 -0600

    day 7.  (removed timer in pt 2)

--- puzzles/day7/part2.py
+++ puzzles/day7/part2.py
@@ -3,15 +3,12 @@ Day 7 - Part 2
 """
 
 from puzzles import Puzzle
-import time
 
 puzzle = Puzzle(__file__)
 nums = puzzle.comma_line_to_ints()
 
 
 def run():
-    t = time.localtime()
-    print(f'{t.tm_hour}:{t.tm_min}:{t.tm_sec}')
     min_n, max_n = min(nums), max(nums)
     result = sum([sum(range(1, abs(n - 0) + 1)) for n in nums])  # Start with 0
 
@@ -19,8 +16,6 @@ def run():
         fuel_sum = sum([sum(range(1, abs(n - i) + 1)) for n in nums])
         result = fuel_sum if fuel_sum < result else result
 
-    t = time.localtime()
-    print(f'{t.tm_hour}:{t.tm_min}:{t.tm_sec}')
     return result
 
 
